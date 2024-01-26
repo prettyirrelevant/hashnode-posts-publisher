f08ff0a96475f50fcd2621926c0e7b71e9c7e5cc
Isaac Adewumi <ienioladewumi@gmail.com>
2024-01-26 13:08:06 +0100

    chore: update dependencies in package.json

--- dist/index.js
+++ dist/index.js
@@ -32628,176 +32628,6 @@ exports.debug = debug; // for test
 
 /***/ }),
 
-/***/ 9818:
-/***/ ((__unused_webpack_module, exports) => {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-
-var highlightRegExp = /highlight-(?:text|source)-([a-z0-9]+)/;
-
-function highlightedCodeBlock (turndownService) {
-  turndownService.addRule('highlightedCodeBlock', {
-    filter: function (node) {
-      var firstChild = node.firstChild;
-      return (
-        node.nodeName === 'DIV' &&
-        highlightRegExp.test(node.className) &&
-        firstChild &&
-        firstChild.nodeName === 'PRE'
-      )
-    },
-    replacement: function (content, node, options) {
-      var className = node.className || '';
-      var language = (className.match(highlightRegExp) || [null, ''])[1];
-
-      return (
-        '\n\n' + options.fence + language + '\n' +
-        node.firstChild.textContent +
-        '\n' + options.fence + '\n\n'
-      )
-    }
-  });
-}
-
-function strikethrough (turndownService) {
-  turndownService.addRule('strikethrough', {
-    filter: ['del', 's', 'strike'],
-    replacement: function (content) {
-      return '~' + content + '~'
-    }
-  });
-}
-
-var indexOf = Array.prototype.indexOf;
-var every = Array.prototype.every;
-var rules = {};
-
-rules.tableCell = {
-  filter: ['th', 'td'],
-  replacement: function (content, node) {
-    return cell(content, node)
-  }
-};
-
-rules.tableRow = {
-  filter: 'tr',
-  replacement: function (content, node) {
-    var borderCells = '';
-    var alignMap = { left: ':--', right: '--:', center: ':-:' };
-
-    if (isHeadingRow(node)) {
-      for (var i = 0; i < node.childNodes.length; i++) {
-        var border = '---';
-        var align = (
-          node.childNodes[i].getAttribute('align') || ''
-        ).toLowerCase();
-
-        if (align) border = alignMap[align] || border;
-
-        borderCells += cell(border, node.childNodes[i]);
-      }
-    }
-    return '\n' + content + (borderCells ? '\n' + borderCells : '')
-  }
-};
-
-rules.table = {
-  // Only convert tables with a heading row.
-  // Tables with no heading row are kept using `keep` (see below).
-  filter: function (node) {
-    return node.nodeName === 'TABLE' && isHeadingRow(node.rows[0])
-  },
-
-  replacement: function (content) {
-    // Ensure there are no blank lines
-    content = content.replace('\n\n', '\n');
-    return '\n\n' + content + '\n\n'
-  }
-};
-
-rules.tableSection = {
-  filter: ['thead', 'tbody', 'tfoot'],
-  replacement: function (content) {
-    return content
-  }
-};
-
-// A tr is a heading row if:
-// - the parent is a THEAD
-// - or if its the first child of the TABLE or the first TBODY (possibly
-//   following a blank THEAD)
-// - and every cell is a TH
-function isHeadingRow (tr) {
-  var parentNode = tr.parentNode;
-  return (
-    parentNode.nodeName === 'THEAD' ||
-    (
-      parentNode.firstChild === tr &&
-      (parentNode.nodeName === 'TABLE' || isFirstTbody(parentNode)) &&
-      every.call(tr.childNodes, function (n) { return n.nodeName === 'TH' })
-    )
-  )
-}
-
-function isFirstTbody (element) {
-  var previousSibling = element.previousSibling;
-  return (
-    element.nodeName === 'TBODY' && (
-      !previousSibling ||
-      (
-        previousSibling.nodeName === 'THEAD' &&
-        /^\s*$/i.test(previousSibling.textContent)
-      )
-    )
-  )
-}
-
-function cell (content, node) {
-  var index = indexOf.call(node.parentNode.childNodes, node);
-  var prefix = ' ';
-  if (index === 0) prefix = '| ';
-  return prefix + content + ' |'
-}
-
-function tables (turndownService) {
-  turndownService.keep(function (node) {
-    return node.nodeName === 'TABLE' && !isHeadingRow(node.rows[0])
-  });
-  for (var key in rules) turndownService.addRule(key, rules[key]);
-}
-
-function taskListItems (turndownService) {
-  turndownService.addRule('taskListItems', {
-    filter: function (node) {
-      return node.type === 'checkbox' && node.parentNode.nodeName === 'LI'
-    },
-    replacement: function (content, node) {
-      return (node.checked ? '[x]' : '[ ]') + ' '
-    }
-  });
-}
-
-function gfm (turndownService) {
-  turndownService.use([
-    highlightedCodeBlock,
-    strikethrough,
-    tables,
-    taskListItems
-  ]);
-}
-
-exports.gfm = gfm;
-exports.highlightedCodeBlock = highlightedCodeBlock;
-exports.strikethrough = strikethrough;
-exports.tables = tables;
-exports.taskListItems = taskListItems;
-
-
-/***/ }),
-
 /***/ 4800:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
@@ -34436,8 +34266,6 @@ const glob = __importStar(__nccwpck_require__(8090));
 const path = __importStar(__nccwpck_require__(9411));
 const front_matter_1 = __importDefault(__nccwpck_require__(7646));
 const turndown_1 = __importDefault(__nccwpck_require__(4800));
-// @ts-ignore
-const turndown_plugin_gfm_1 = __importDefault(__nccwpck_require__(9818));
 const fs = __importStar(__nccwpck_require__(7561));
 /**
  * The main function for the action.
@@ -34458,7 +34286,6 @@ async function run() {
         const globber = await glob.create(patterns.join('\n'));
         // turndown
         const turndownService = new turndown_1.default();
-        turndownService.use(turndown_plugin_gfm_1.default.gfm);
         const posts = [];
         for await (const file of globber.globGenerator()) {
             console.log(`File: ${file}`);
