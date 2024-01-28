a41e63d970d5e16b2694d6ee942caad8fdc885a5
Isaac Adewumi <ienioladewumi@gmail.com>
2024-01-28 08:41:35 +0100

    chore: add console log statement to display the number of successfully uploaded posts

--- src/main.ts
+++ src/main.ts
@@ -70,6 +70,7 @@ export async function run(): Promise<void> {
 
     // TODO: write successful results to lockfile
     const successfulResults = results.filter((result) => result.status === 'fulfilled')
+    console.log(`Successfully uploaded ${successfulResults.length} posts.`)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
